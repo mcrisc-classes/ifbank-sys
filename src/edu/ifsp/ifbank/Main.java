76870fa302210ce3497fe9503f2d9a7ace9f534f
Marcelo Criscuolo <criscuolo@ifsp.edu.br>
2022-11-09 08:43:57 -0300

    remoção código de testes

--- src/edu/ifsp/ifbank/Main.java
+++ src/edu/ifsp/ifbank/Main.java
@@ -1,14 +1,10 @@
 package edu.ifsp.ifbank;
 
 import edu.ifsp.ifbank.gui.MainController;
-import edu.ifsp.ifbank.gui.UseCase;
 
 public class Main {
 	public static void main(String[] args) {
 		MainController mainController = new MainController();
 		mainController.init();
-		
-		/* temporário (desenvolvimento) */
-		// mainController.start(UseCase.CADASTRO_CLIENTE);
 	}
 }
